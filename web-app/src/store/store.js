afc41ec7bf676b8e5d072a211487fd133f7d1c33
Amlan <amlannandy5@gmail.com>
2021-01-24 08:27:47 +0530

    Added necessary packages

---
+++ web-app/src/store/store.js
@@ -0,0 +1,23 @@
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import auth from './reducers/auth';
+import alert from './reducers/alert';
+
+const rootReducer = combineReducers({
+  auth: auth,
+  alert: alert,
+});
+
+const initialState = {};
+
+const middleWare = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+
+export default store;
