1caa409befc5d9474562e7437b73d8ed7a3a9601
Amlan <amlannandy5@gmail.com>
2021-01-24 10:37:22 +0530

    completed auth routes

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
@@ -19,5 +21,27 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// Encrypt password
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// Match user entered password with hashed password in db
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = User = mongoose.model('user', UserSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,26 +1,142 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: 'Get current user route',
-  });
+router.get('/', async (req, res) => {
+  try {
+    let token;
+    // Get token from headers
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+    // Make sure token exists
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        errors: [{ msg: 'Not authorized to use this route ' }],
+      });
+    }
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        errors: [{ msg: 'Not authorized to use this route ' }],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {
+        token,
+        user,
+      },
+    });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).json({
+      success: false,
+      errors: [{ msg: 'Internal Server Error' }],
+    });
+  }
 });
 
-router.post('/login', (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: 'Login route',
-  });
-});
+router.post(
+  '/login',
+  [
+    check('email', 'Invalid Email').isEmail(),
+    check('password', 'Provide a password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        errors: errors.array(),
+      });
+    }
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User doesn't exist" }] });
+      }
+      //Check password
+      const isMatch = await user.matchPassword(password);
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Wrong password' }] });
+      }
+      const token = user.getSignedJWTToken();
+      res.status(200).json({
+        success: true,
+        data: {
+          token,
+          user,
+        },
+      });
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).json({
+        success: false,
+        errors: [{ msg: 'Internal Server Error' }],
+      });
+    }
+  }
+);
 
-router.post('/register', (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    data: 'Register route',
-  });
-});
+router.post(
+  '/register',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Invalid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      //Check if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      user = new User({ name, email, password });
+      await user.save();
+      const token = user.getSignedJWTToken();
+      res.status(200).json({
+        success: true,
+        data: {
+          token,
+          user,
+        },
+      });
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).json({
+        success: false,
+        errors: [{ msg: 'Internal Server Error' }],
+      });
+    }
+  }
+);
 
 module.exports = router;
