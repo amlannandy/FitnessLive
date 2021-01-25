624e4bced6b44ca33551df62b467e58ad33a02d0
Amlan <amlannandy5@gmail.com>
2021-01-25 19:33:59 +0530

    Fixed routing issue

--- web-app/src/routes/HomeRoutes.js
+++ web-app/src/routes/HomeRoutes.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Switch } from 'react-router-dom';
-
-import PrivateRoute from './PrivateRoute';
+import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../views/Dashboard';
 import Employees from '../views/Employees';
@@ -9,8 +7,8 @@ import Employees from '../views/Employees';
 const HomeRoutes = ({ url }) => {
   return (
     <Switch>
-      <PrivateRoute exact path={url + '/'} component={Dashboard} />
-      <PrivateRoute exact path={url + 'employees'} component={Employees} />
+      <Route exact path={url + '/'} component={Dashboard} />
+      <Route exact path={url + 'employees'} component={Employees} />
     </Switch>
   );
 };
