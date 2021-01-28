9b45a93e6631736f6172fa7adc3eeadf2deca40e
Amlan <amlannandy5@gmail.com>
2021-01-28 13:07:00 +0530

    completed google fit integration

--- mobile-app/lib/screens/GoogleFitScreen.dart
+++ mobile-app/lib/screens/GoogleFitScreen.dart
@@ -1,7 +1,9 @@
 import 'package:fit_kit/fit_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 import '../widgets/CustomAppBar.dart';
+import '../widgets/CustomGridItem.dart';
 
 class GoogleFitScreen extends StatefulWidget {
   @override
@@ -9,60 +11,121 @@ class GoogleFitScreen extends StatefulWidget {
 }
 
 class _GoogleFitScreenState extends State<GoogleFitScreen> {
+  double _height = 175.4, _weight = 71.2, _distance = 4.56, _heartRate = 61;
+
   @override
   void initState() {
-    hasPermissions();
+    _requestPermission();
     super.initState();
   }
 
-  Future<void> hasPermissions() async {
-    // try {
-    //   var permissions = await FitKit.hasPermissions(DataType.values);
-    // } catch (e) {
-    //   var result = 'hasPermissions: $e';
-    // }
-
-    // if (!mounted) return;
-
-    // setState(() {});
+  void _requestPermission() async {
+    final result = await FitKit.hasPermissions([
+      DataType.STEP_COUNT,
+      DataType.DISTANCE,
+      DataType.EXERCISE_TIME,
+      DataType.STAND_TIME,
+      DataType.HEART_RATE,
+      DataType.HEIGHT,
+      DataType.WATER,
+    ]);
+    print('Permission Result - ' + result.toString());
+    _getData();
   }
 
-  void _requestPermission() async {
-    await FitKit.hasPermissions([
-      DataType.HEART_RATE,
-      DataType.STEP_COUNT,
-      DataType.HEIGHT,
-      DataType.WEIGHT,
+  void _getData() async {
+    final distanceData = await FitKit.read(
       DataType.DISTANCE,
-      DataType.ENERGY,
-      DataType.WATER
-    ]);
+      dateFrom: DateTime.now().subtract(Duration(days: 1)),
+      dateTo: DateTime.now(),
+      limit: 1,
+    );
+    setState(() => _distance = distanceData[0].value);
+    final heightData = await FitKit.read(
+      DataType.HEIGHT,
+      dateFrom: DateTime.now().subtract(Duration(days: 1)),
+      dateTo: DateTime.now(),
+      limit: 1,
+    );
+    setState(() => _height = heightData[0].value);
+    final weightData = await FitKit.read(
+      DataType.WEIGHT,
+      dateFrom: DateTime.now().subtract(Duration(days: 1)),
+      dateTo: DateTime.now(),
+      limit: 1,
+    );
+    setState(() => _weight = weightData[0].value);
+    final heartRateData = await FitKit.read(
+      DataType.HEART_RATE,
+      dateFrom: DateTime.now().subtract(Duration(days: 1)),
+      dateTo: DateTime.now(),
+      limit: 1,
+    );
+    setState(() => _heartRate = heartRateData[0].value);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar('Google Fit'),
+      appBar: CustomAppBar(''),
       backgroundColor: Colors.white,
-      body: FutureBuilder<List<FitData>>(
-        future: FitKit.read(
-          DataType.DISTANCE,
-          dateFrom: DateTime.now().subtract(Duration(days: 1)),
-          dateTo: DateTime.now(),
-          limit: 1,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/googlefit.png',
+              height: 100,
+            ),
+            SizedBox(height: 15),
+            Text(
+              'Data for last 24 hours',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.7),
+                fontFamily: 'Varela',
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(height: 15),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              padding: const EdgeInsets.all(15),
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                children: [
+                  CustomGridItem(
+                    title: 'Heart Rate',
+                    subtitle: '${_heartRate.toStringAsFixed(0)} bpm',
+                    icon: LineIcons.heart_o,
+                    color: Colors.red,
+                  ),
+                  CustomGridItem(
+                    title: 'Distance',
+                    subtitle: '${_distance.toStringAsFixed(2)} km',
+                    icon: Icons.directions_walk,
+                    color: Colors.yellow[700],
+                  ),
+                  CustomGridItem(
+                    title: 'Height',
+                    subtitle: '${_height.toStringAsFixed(2)} cm',
+                    icon: LineIcons.sort_numeric_asc,
+                    color: Colors.blue,
+                  ),
+                  CustomGridItem(
+                    title: 'Weight',
+                    subtitle: '${_weight.toStringAsFixed(2)} kg',
+                    icon: LineIcons.balance_scale,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting ||
-              !snapshot.hasData) {
-            return Container();
-          }
-          final data = snapshot.data[0].toString();
-          return Column(
-            children: [
-              Text(data),
-            ],
-          );
-        },
       ),
     );
   }
