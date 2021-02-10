41754bec0afeff0bc9a6fcbb665e9a7a39d2c263
Amlan <amlannandy5@gmail.com>
2021-02-10 19:59:44 +0530

    Enhanced enquiry section

--- mobile-app/lib/screens/EnquiryScreen.dart
+++ mobile-app/lib/screens/EnquiryScreen.dart
@@ -22,8 +22,23 @@ class _EnquiryScreenState extends State<EnquiryScreen> {
     Message(message: 'Enter name of the record you\'re looking for'),
   ];
 
+  void _sendHealthData() {
+    final response = Message(
+      message:
+          'Your current health data:\n1. Heart Rate - 18bpm\n2. Blood Pressure - 60/90 mmHg\n3. Glucose - 115 mg/dL\n4. Respiration - 21 bpm\n5. Body Temperature - 37C\n6. Oxygen Saturation - 98%\n7. Electro Cardiogram - 162 ms\n8. Steps - 3855',
+    );
+    setState(() => _messages.add(response));
+  }
+
   void _onSend(String userId) async {
     String text = _answerController.text;
+    if (text == 'Show tracker data') {
+      Future.delayed(Duration(seconds: 2), () {
+        _sendHealthData();
+        _answerController.clear();
+      });
+      return;
+    }
     setState(() {
       _messages.add(Message(
         message: text,
