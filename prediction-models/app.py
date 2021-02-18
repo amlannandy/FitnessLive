43994558305710b0ff73b9315948e0f5dc546bdc
Amlan <amlannandy5@gmail.com>
2021-02-18 14:49:37 +0530

    Created diabetes route

--- prediction-models/app.py
+++ prediction-models/app.py
@@ -1,18 +1,29 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
-import pickle
-import numpy as np
-from model import NLPModel
+from flask import Flask, jsonify
+from scripts.diabetes import run_diabetes_model
 
 app = Flask(__name__)
-api = Api(app)
-# create new model object
-model = NLPModel()
-# load trained classifier
-clf_path = 'models/diabetes.pkl'
-with open(clf_path, 'rb') as f:
-    model.clf = pickle.load(f)
-# load trained vectorizer
-vec_path = 'lib/models/TFIDFVectorizer.pkl'
-with open(vec_path, 'rb') as f:
-    model.vectorizer = pickle.load(f)
+
+@app.route('/')
+def home():
+   return 'Flask server is working!', 200
+
+@app.route('/diabetes')
+def run_diabetes():
+    try:
+        res = run_diabetes_model()
+        response = {
+            'success': True,
+            'result': res,
+        }
+        return jsonify(response), 200
+    except:
+        response = {
+            'success': False,
+            'msg': 'Server Error'
+        }
+        return jsonify(response), 500
+
+@app.route('/coronary-heart-disease')
+def run_chd():
+   pass
+
